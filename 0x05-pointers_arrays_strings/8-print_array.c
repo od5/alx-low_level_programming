127165a63989da93564045832f70846a42cbf0fc
od5 <od5_agbo@yahoo.com>
2023-01-04 21:26:21 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,17 +1,27 @@
+#include "holberton.h"
 #include <stdio.h>
+
 /**
-* print_array - print n of int array
-* @a: pointer
-* @n: n
-*/
+ * print_array - print `n` elements of an array of integers
+ * @a: int type array pointer
+ * @n: int type integer
+ * Description: Numbers must be separated by comma and space.
+ * Numbers should be displayed in the same order they are stored in array.
+ * You can only use _putchar to print.
+ */
+
 void print_array(int *a, int n)
 {
-	short c = 0;
-	while (n-- > 0)
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; n--, i++)
 	{
-		printf("%d", a[c++]);
-		if (n != 0)
+		printf("%d", a[i]);
+		if (n > 0)
+		{
 			printf(", ");
+		}
 	}
-		printf("\n");
+	printf("\n");
 }
