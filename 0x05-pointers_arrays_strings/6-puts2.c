37ee50f72ed4c01cdd77821fbd03cc663f426e88
od5 <od5_agbo@yahoo.com>
2023-01-04 21:11:53 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,22 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * puts2 - prints one char out of 2
- * @str: type
- *
+ * puts2 - prints xters of a string
+ * @str: the used string reference
+ * Return: 0
  */
 
 void puts2(char *str)
 {
-	int i, j = 0;
+	int i = 0;
 
 	while (str[i] != '\0')
 	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 		i++;
-	}
-
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
 	}
 	_putchar('\n');
 }
